15f58774951393c0f38503adf061dcea8f327f4c
masnzzz <z3heng0326@gmail.com>
2023-02-05 14:41:09 +0900

    feat: Profileコンポーネントを作成

---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div>
+      <h3>Profile</h3>
+      <ul>
+        <li>名前: Masana</li>
+        <li>Lv: 999</li>
+        <li>職業: フロントエンドエンジニア</li>
+        <li>趣味: ドカ食い</li>
+        <li>似ている動物: メガロドン</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
